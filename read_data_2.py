3e4d7dac3bb922498f26ec8c229ce539693db3c2
meteoNL <cirrocumulus8@gmail.com>
2018-10-21 10:42:31 +0000

    better documentation read data 2

--- read_data_2.py
+++ read_data_2.py
@@ -10,15 +10,15 @@ import matplotlib as mpl
 import matplotlib.pyplot as pl
 pl.close("all")
 mpl.rcParams.update({'font.size': 21})
-r=6.37e6
-ndays=365.
+r=6.37e6 #radius of earth in m
+ndays=365. #number of days in a normal year
 navg=96 #nbumber of hours to average signal 
-nhours=24.
-ndeg=360.
-radconv=ndeg/(2.*np.pi)
-add_date=np.array([0,31,59,90,120,151,181,212,243,273,304,334])
-crit_std=10.0
-crit_dt=1.25
+nhours=24. #number of hours per day
+ndeg=360. #number of degrees in a circle
+radconv=ndeg/(2.*np.pi) #conversion between degrees and radians
+add_date=np.array([0,31,59,90,120,151,181,212,243,273,304,334]) #day of year for each starting day of a month
+crit_std=10.0 #critical number of standard deviations for filtering applications
+crit_dt=1.25 #critical relative (to navg) time interval to accept a velocity sample
 
 #%% read data
 #filename
@@ -69,22 +69,34 @@ lats=lats[lons<0]
 lons=lons[lons<0]
     
 def filter_dataset():
+    #calculate mean and std dev for filtering dataset based on height + set counter
     mean_height=np.mean(height)
     std_height=np.std(height)
     counter=0
+    
+    #repeated check for the need to be filtered
     while np.max(np.abs(height-mean_height)/std_height) > crit_std:
         print(counter)
+        
+        #pass along all samples
         for i in range(len(height)):
             height_i=height[i]
+            
+            #replace all position values by linear interpolation between nearest neighbours
             if np.abs(height_i-mean_height)/std_height > crit_std: 
                 height_i=0.5*height[(i-1)]+0.5*height[(i+1)]
                 lat_i=0.5*lats[(i-1)]+0.5*lats[(i+1)]
                 lon_i=0.5*lons[(i-1)]+0.5*lons[(i+1)]
+                
+                #update with obtained values in array
                 lons[i],lats[i],height[i]=lon_i,lat_i,height_i
+        
+        #update statistics subsequently
         mean_height=np.mean(height)
         std_height=np.std(height)
         counter+=1
 
+#apply filter function on dataset
 filter_dataset()
 
 #%% velocity computation function
@@ -108,7 +120,7 @@ def compute_velocities(nstep,lons,lats,time,height):
         v=dy/dt
         velocity=(u**2.+v**2.)**(0.5)
         
-        #put result in an array (inculding time)
+        #put result in an array (inculding time) if it fulfills the time step criterion
         if dt < (crit_dt*navg/(ndays*nhours)) and dt > 0.00:
             raw_velocities=np.append(raw_velocities,np.array([time_t,u,v,velocity]))
     return raw_velocities[0::4],raw_velocities[1::4],raw_velocities[2::4],raw_velocities[3::4]
@@ -126,15 +138,19 @@ pl.show()
 #%%
 def averagingcomputation():
     
-    #create arrays
+    #create arrays to save values
     lonsavg=np.array([])
     latsavg=np.array([])
     zavg=np.array([])
     tavg=np.array([])
     
     for i in range(0,int(len(lons)/navg)):
+        
+        #get the subarrays
         startind=i*navg
         endind=(i+1)*navg
+        
+        #fill the new arrays with averages based on subarray
         lonsavg=np.append(lonsavg,np.mean(lons[startind:endind]))
         latsavg=np.append(latsavg,np.mean(lats[startind:endind]))
         zavg=np.append(zavg,np.mean(height[startind:endind]))
@@ -168,6 +184,7 @@ def postproc():
         checkarray+=1
         print(checkarray)
         for i in checkarray:
+            #interpolate values of velocity based on nearest neighbours and use this to calculate a translation of the sensor for both lat and lon (only one neighbour in i = 0 or i = arraylength case)
             if i > 1 and i < len(lonsavg):
                 trans_xcor=0.5*(lonsavg[i-2]-lonsavg[i-1]+lonsavg[i+1]-lonsavg[i])
                 trans_ycor=0.5*(latsavg[i-2]-latsavg[i-1]+latsavg[i+1]-latsavg[i])
@@ -177,10 +194,16 @@ def postproc():
             elif i == len(lonsavg):
                 trans_xcor=lonsavg[i-2]-lonsavg[i-1]
                 trans_ycor=latsavg[i-2]-latsavg[i-1]
+            
+            #do the translation on the full array after the jump based on two coordinate difference and its expected value based on interpolated velocity
             trans_x=lonsavg[i]-lonsavg[i-1]-trans_xcor
             trans_y=latsavg[i]-latsavg[i-1]-trans_ycor
+            
+            #translation
             lonsavg[i:]=lonsavg[i:]-trans_x
             latsavg[i:]=latsavg[i:]-trans_y
+        
+        #update old values for new statistics based on newly obtained arrays and do counter
         gradlatsavg=latsavg[1:]-latsavg[:-1]
         gradlonsavg=lonsavg[1:]-lonsavg[:-1]
         meangradlat=np.mean(gradlatsavg[:])
@@ -190,7 +213,8 @@ def postproc():
         counter+=1
         print(counter)
     return lonsavg,latsavg
-        
+
+#the execution of averaging and post-processing        
 lonsavg,latsavg,zavg,tavg=averagingcomputation()
 lonsavg,latsavg=postproc()
 timeval,uval,vval,velval=compute_velocities(1,lonsavg,latsavg,tavg,zavg)
@@ -213,6 +237,7 @@ pl.ylabel('Velocity (m/yr)')
 pl.xlabel('Time (yr)')
 pl.show()
 
+#writing result to file (if uncommented)
 sep=';'
 #g=open(fn[:-4]+'.csv','a')
 #for i in range(len(timeval)-1):
