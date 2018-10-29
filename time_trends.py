ba55e9a522f17f0c4cd4958560a2a0722b6cd146
meteoNL <cirrocumulus8@gmail.com>
2018-10-29 14:32:23 +0000

    seasonal trends implemented

--- time_trends.py
+++ time_trends.py
@@ -22,6 +22,14 @@ names=['SHR','S4','S5','S6','S7','S8','S9','S10'] #names of sites
 start=2006 #start year
 end=2019 #end year plus one
 min_length=81.5 #length of observations per year criterion, in number of 96 hours intervals 
+season='annual'
+seasonbgn=-1./3
+seasonend=8./12
+pl.figure(figsize=(12,8))
+if season != 'annual':
+    ender=-1
+else: 
+    ender=0
 
 #empty dictionary
 values={}
@@ -53,10 +61,9 @@ def linreg(time,vel):
 #96 h time series time trend
 for key in values:
     time=values[key][:,0]
-    vel=values[key][:,2]
+    vel=values[key][:,3]
     lin_time_trend=linreg(time,vel)
-print('annual trends')
-
+print(season+'trends')
 pl.figure(figsize=(12,8))
 #annual velocities time trend  
 j=0  
@@ -73,8 +80,8 @@ for key in names:
     for y in range(start,end):
         
         #selection of velocity data per glaciological/mass balance year (??) year
-        first=y-1./3
-        last=y+2./3
+        first=y+seasonbgn
+        last=y+seasonend
         vel_yr=vel[time>first]
         time_yr=time[time>first]
         
@@ -86,9 +93,13 @@ for key in names:
         i+=1
       #  print(key,y,vel_yr)
     if key == 'S5' or key == 'S6':
-        fn='iwsannual'+key+'.npy'
-        new_vel[-3:-1]=np.load(fn)[:,1]
-        new_time[-3:-1]=np.array([2016.,2017.])
+        fn='iws'+season+key+'.npy'
+        if season == 'annual':
+            new_vel[-3:-1]=np.load(fn)[:2,1]
+            new_time[-3:-1]=np.array([2016.,2017.])
+        else:
+            new_vel[-3:]=np.load(fn)[:,1]
+            new_time[-3:]=np.array([2016.,2017.,2018.])
     #remove zeros
     new_vel=new_vel[new_time>0]
     new_time=new_time[new_time>0]
@@ -101,10 +112,10 @@ for key in names:
     linreg(new_time,new_vel)
     j+=1
             
-pl.title('Annual average velocity series')
+pl.title(season+' average velocity series')
 pl.xlabel('Time (yr)')
 pl.ylabel('Velocity (m/yr)')
-pl.ylim(0,150)
+pl.ylim(0,180)
 pl.grid()
 pl.xlim(2002,2019)
 pl.legend(loc=2)
